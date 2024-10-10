e75e9f119118603ea6acb3414d2ad5532e87b9ff
ClaytonAndrade <clayton_m_andrade@hotmail.com>
2024-10-10 20:31:36 -0300

    Implementando Cadastro Tecnico parte1

---
+++ src/main/java/br/com/professorclaytonandrade/sistemaservicosjavafx/controller/CadastroTecnicoController.java
@@ -0,0 +1,49 @@
+package br.com.professorclaytonandrade.sistemaservicojavafx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CadastroTecnicoController {
+
+    @FXML
+    private Button cancelarButton;
+
+    @FXML
+    private TextField cpfField;
+
+    @FXML
+    private DatePicker dataCriacaoField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField nomeField;
+
+    @FXML
+    private TextField salarioField;
+
+    @FXML
+    private Button salvarButton;
+
+    @FXML
+    private PasswordField senhaField;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvar(ActionEvent event) {
+
+    }
+
+}
